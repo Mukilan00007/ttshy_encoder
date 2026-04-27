665cfdd680cd1f2895d133ca9775f3b1cd9d23c0
Mukilan Ramakrishnan <mukilanramays2007@gmail.com>
2026-04-27 10:09:27 +0530

    Refactor testbench for encoder 256to8 module

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,46 @@
-`default_nettype none
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module encoder_256to8_tb;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+reg  [255:0] in;
+wire [7:0] out;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+encoder_256to8 uut (
+    .in(in),
+    .out(out)
+);
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+initial begin
+    $display("Time\tOutput");
+    $monitor("%0t\t%d", $time, out);
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+    // Test cases
+    in = 256'b0; 
+    in[0]   = 1'b1; #10;
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    in = 256'b0; 
+    in[1]   = 1'b1; #10;
+
+    in = 256'b0; 
+    in[2]   = 1'b1; #10;
+
+    in = 256'b0; 
+    in[10]  = 1'b1; #10;
+
+    in = 256'b0; 
+    in[50]  = 1'b1; #10;
+
+    in = 256'b0; 
+    in[100] = 1'b1; #10;
+
+    in = 256'b0; 
+    in[200] = 1'b1; #10;
+
+    in = 256'b0; 
+    in[255] = 1'b1; #10;
+
+    $finish;
+end
 
 endmodule
+
