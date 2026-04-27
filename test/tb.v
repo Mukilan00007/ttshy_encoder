b4e136eba1ad3da6478fbff947c0b555d49558cc
Mukilan Ramakrishnan <mukilanramays2007@gmail.com>
2026-04-27 11:57:32 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,7 +1,6 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-// This MUST be named "tb" to match the Makefile
 module tb ();
 
   // Standard Tiny Tapeout pins
@@ -14,7 +13,11 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Signal for power pins (required for Gate Level Simulation)
+  // 1. ADD THESE POWER WIRES
+  wire vcc = 1'b1;
+  wire gnd = 1'b0;
+
+  // Initial values
   initial begin
       clk = 0;
       rst_n = 0;
@@ -23,11 +26,11 @@ module tb ();
       uio_in = 0;
   end
 
-  // Instantiate your module
+  // 2. UPDATE THE INSTANTIATION
   tt_um_encoder_256to8 user_project (
 `ifdef GL_TEST
-      .VPWR(1'b1),  // Power connections for Gate Level
-      .VGND(1'b0),
+      .VPWR(vcc),      // Connect to the wire, not the constant 1'b1
+      .VGND(gnd),      // Connect to the wire, not the constant 1'b0
 `endif
       .ui_in  (ui_in),
       .uo_out (uo_out),
